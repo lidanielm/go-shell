80581a320cf3e4e82a952ce51d7356edcd9a83db
DLi2004 <danielmli2023@gmail.com>
2024-06-13 18:20:13 -0400

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -16,7 +16,13 @@ func main() {
 	for running {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		fmt.Fprint(os.Stdout, input[:len(input)-1] + ": command not found\n")
+		var valid_command = false
+		if valid_command {
+			fmt.Fprint(os.Stdout, "Command executed\n")
+		} else {
+			fmt.Fprint(os.Stdout, input[:len(input)-1] + ": command not found\n")	
+		}
+		
 	}
 
 }
