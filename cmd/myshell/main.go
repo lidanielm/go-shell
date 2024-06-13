2eb18c2896fad0f97cd6d864cd15ae3eb42dcc75
DLi2004 <danielmli2023@gmail.com>
2024-06-13 18:12:57 -0400

    handle missing commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -16,7 +16,7 @@ func main() {
 	for running {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		fmt.Fprint(os.Stdout, input[:len(input)-1] + ": command not found")
+		fmt.Fprint(os.Stdout, input[:len(input)-1] + ": command not found\n")
 	}
 
 }
