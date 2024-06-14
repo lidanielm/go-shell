d43f744da7868ae792e14e9a5545304ab9bf1918
DLi2004 <danielmli2023@gmail.com>
2024-06-13 21:59:05 -0400

    echo

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,19 +5,23 @@ import (
 	// Uncomment this block to pass the first stage
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	// Uncomment this block to pass the first stage
 	// fmt.Fprint(os.Stdout, "$ ")	
 		
-	var running = true
+	running := true
 	// Wait for user input
 	for running {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		args := strings.Fields(input)
 		if input == "exit 0\n" {
 			running = false
+		} else if args[0] == "echo" {
+			fmt.Fprint(os.Stdout, strings.Join(args[1:], " ") + "\n")
 		} else {
 			fmt.Fprint(os.Stdout, input[:len(input) - 1] + ": command not found\n")	
 		}
