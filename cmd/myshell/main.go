d8378df79a519de812e1ced63bff2222b37c7638
DLi2004 <danielmli2023@gmail.com>
2024-06-14 11:26:32 -0400

    type executable

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	// Uncomment this block to pass the first stage
 	// fmt.Fprint(os.Stdout, "$ ")	
-		
+	
+	path := os.Args[0]
 	running := true
 	commands := []string{"echo", "type", "exit"}
 	// Wait for user input
@@ -25,11 +26,17 @@ func main() {
 			fmt.Fprint(os.Stdout, strings.Join(args[1:], " ") + "\n")
 		} else if args[0] == "type" {
 			found := false
+			// Check if the command is a shell builtin
 			for _, command := range commands {
 				if args[1] == command {
 					fmt.Fprint(os.Stdout, command + " is a shell builtin\n")
 					found = true
 				}
+			}
+			// Check if the file exists
+			if fileExists(path, args[1]) {
+				fmt.Fprint(os.Stdout, args[1] + " is " + path + "\n")
+				found = true
 			}
 			if !found {
 				fmt.Fprint(os.Stdout, args[1] + ": not found\n")
@@ -40,4 +47,17 @@ func main() {
 		
 	}
 
+}
+
+func fileExists(path string, cmd string) bool {
+	files, _ := os.ReadDir(path)
+	for _, file := range files {
+		if file.Name() == cmd {
+			return true
+		}
+		if file.IsDir() {
+			return fileExists(path + "/" + file.Name())
+		}
+	}
+	return false
 }
